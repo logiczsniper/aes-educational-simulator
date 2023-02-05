19e895d10e3940141c585d81bfcf64207350f88e
logiczsniper <lczernel@gmail.com>
2023-02-05 15:11:19 +0000

    Refactored LanguageSelect to have a skeleton loading animation

--- aes-adventure-ui/components/LanguageSelect.vue
+++ aes-adventure-ui/components/LanguageSelect.vue
@@ -10,8 +10,13 @@ watch(selectedLocaleCode, newLocaleCode => {
 </script>
 
 <template>
-  <ClientOnly>
-    <div class="languageSelect">
+  <div class="languageSelect">
+    <v-icon
+      class="translateIcon"
+      icon="mdi-translate-variant"
+      size="18"
+    />
+    <ClientOnly>
       <v-select
         v-model="selectedLocaleCode"
         class="selectComponent"
@@ -25,13 +30,6 @@ watch(selectedLocaleCode, newLocaleCode => {
           closeOnContentClick: true,
         }"
       >
-        <template #prepend>
-          <v-icon
-            class="translateIcon"
-            icon="mdi-translate-variant"
-            size="18"
-          />
-        </template>
         <template #item="{ item: { raw: { code, name, emoji } } }">
           <v-btn
             variant="plain"
@@ -62,85 +60,59 @@ watch(selectedLocaleCode, newLocaleCode => {
           </v-btn>
         </template>
       </v-select>
-      <v-btn
-        variant="plain"
-        size="x-small"
-        class="languageHelp"
-      >
-        <!-- TODO: link to GitLab README explaining how to contribute -->
-        <p><small>{{ t('home.settings.badLang') }}</small></p>
-      </v-btn>
-    </div>
-    <template #fallback>
-      <div class="languageSelect">
-        <div class="test">
-          <v-icon
-            class="translateIcon"
-            icon="mdi-translate-variant"
-            size="18"
-          />
-          <div class="selectComponent skeleton">
-          </div>
-        </div>
-        <v-btn
-          variant="plain"
-          size="x-small"
-          class="languageHelp"
-          disabled
-        >
-          <!-- TODO: link to GitLab README explaining how to contribute -->
-          <p><small>{{ t('home.settings.badLang') }}</small></p>
-        </v-btn>
-      </div>
-    </template>
-  </ClientOnly>
+      <template #fallback>
+        <div class="selectSkeleton skeleton" />
+      </template>
+    </ClientOnly>
+  </div>
+  <v-btn
+    variant="plain"
+    size="x-small"
+    class="languageHelp"
+  >
+    <!-- TODO: link to GitLab README explaining how to contribute -->
+    <p><small>{{ t('home.settings.badLang') }}</small></p>
+  </v-btn>
 </template>
 
-<!-- TODO: break up this component to have a skeleton, general code cleanup NEEDED -->
-
 <style scoped lang="scss">
-.test {
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  margin-bottom: 22px;
-}
-
 .languageSelect {
+  display: flex;
+  gap: 12px;
+  align-items: center;
+
+  .translateIcon {
+    opacity: 0.6;
+    margin-top: -22px;
+  }
+
   .selectComponent {
     max-width: 50%;
-
-    .translateIcon {
-      margin-top: 8px
-    }
+    display: grid;
+    align-items: center;
 
     .languageItem {
       margin-left: -10px;
-    }
-
-    .v-field {
-      align-items: baseline;
     }
 
     :deep(.v-field__append-inner) {
       align-items: center;
       padding: 0;
     }
-
-
-
-    &.skeleton {
-      width: calc(50% - 18px);
-      margin-left: 18px;
-      height: 52px;
-      border-radius: 4px;
-    }
   }
 
-  .languageHelp {
-    margin-top: -36px;
-    padding-left: 0;
+  .selectSkeleton {
+    @extend .selectComponent;
+    height: 52px;
+    margin-bottom: 22px;
+    border-radius: 4px;
+    width: 50%;
   }
+}
+
+.languageHelp {
+  margin-top: -36px;
+  padding-left: 0;
 }
 
 .emoji {
