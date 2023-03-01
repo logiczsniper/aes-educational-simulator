ab64769f7c11777a765998d6791e6b39b9f615ac
logiczsniper <lczernel@gmail.com>
2023-03-01 23:10:25 +0000

    Add ability to animate individual bytes

--- aes-adventure-ui/utils/animation/addAnimationClasses.ts
+++ aes-adventure-ui/utils/animation/addAnimationClasses.ts
@@ -1,9 +1,8 @@
-export const addAnimationClasses = (divs: Array<HTMLDivElement>, id: string) => {
-  const rowCount = 4
-
+export const addAnimationClasses = (divs: Array<HTMLDivElement>, id: string, rowCount = 4) => {
   const getDivIndexClass = (index: number) => `${id}-${index}`
   const getDivRowClass = (row: number) => `${id}-row-${row}`
   const getDivColumnClass = (column: number) => `${id}-column-${column}`
+  const getSpanClass = (row: number, column: number, child: 1 | 2) => `${id}-row-${row}-column-${column}-span-${child}`
 
   const targetDivs = divs.map((div, index) => {
     const row = Math.floor(index / rowCount)
@@ -11,6 +10,10 @@ export const addAnimationClasses = (divs: Array<HTMLDivElement>, id: string) => 
 
     const targetDiv = div.cloneNode(true) as HTMLDivElement
     targetDiv.classList.add(id, getDivIndexClass(index), getDivRowClass(row), getDivColumnClass(column))
+
+    const [firstByteSpan, secondByteSpan] = targetDiv.children
+    firstByteSpan.classList.add(getSpanClass(row, column, 1))
+    secondByteSpan.classList.add(getSpanClass(row, column, 2))
 
     return targetDiv
   })
@@ -22,6 +25,7 @@ export const addAnimationClasses = (divs: Array<HTMLDivElement>, id: string) => 
     targetIndexClass: (index: number) => `.${getDivIndexClass(index)}`,
     targetRowClass: (row: number) => `.${getDivRowClass(row)}`,
     targetColumnClass: (column: number) => `.${getDivColumnClass(column)}`,
-    targetCoordsClass: (row: number, column: number) => `.${getDivRowClass(row)}.${getDivColumnClass(column)}`
+    targetCoordsClass: (row: number, column: number) => `.${getDivRowClass(row)}.${getDivColumnClass(column)}`,
+    targetSpanClass: (row: number, column: number, child: 1 | 2) => `.${getSpanClass(row, column, child)}`
   }
 }
--- aes-adventure-ui/utils/animation/hexToDivs.ts
+++ aes-adventure-ui/utils/animation/hexToDivs.ts
@@ -1,8 +1,19 @@
 export const hexToDivs = (hex: Array<number> | Uint8Array) => [...hex].map((byte) => {
   const byteDiv = document.createElement('div')
 
-  byteDiv.classList.add('code')
-  byteDiv.textContent = formatHex(byte)
+  // byteDiv.textContent = formatHex(byte)
+  const [firstByte, secondByte] = formatHex(byte)
+
+  const firstByteSpan = document.createElement('span')
+  firstByteSpan.textContent = firstByte
+  firstByteSpan.classList.add('code')
+
+  const secondByteSpan = document.createElement('span')
+  secondByteSpan.textContent = secondByte
+  secondByteSpan.classList.add('code')
+
+  byteDiv.appendChild(firstByteSpan)
+  byteDiv.appendChild(secondByteSpan)
 
   return byteDiv
 })
