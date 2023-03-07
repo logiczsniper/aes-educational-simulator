6d4c72bd6e34d764fc8241c2002f47298b9ad6a9
logiczsniper <lczernel@gmail.com>
2023-03-07 16:59:03 +0000

    Remove test animation and types

--- aes-adventure-ui/utils/animation/types.ts
+++
@@ -1,9 +0,0 @@
-import { AnimeInstance, AnimeTimelineInstance } from "animejs";
-
-interface AesAnimatable {
-  id: string;
-  el: HTMLElement;
-  animeInstance: AnimeInstance;
-}
-
-type AesAnimationFunction = (timelineInstance: AnimeTimelineInstance) => void;
