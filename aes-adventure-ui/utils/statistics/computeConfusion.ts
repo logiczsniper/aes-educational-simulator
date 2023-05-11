94a3281cce535b0de5d486d3e02be389f24f6250
logiczsniper <lczernel@gmail.com>
2023-05-11 01:14:11 +0100

    More confusion work

--- aes-adventure-ui/utils/statistics/computeConfusion.ts
+++ aes-adventure-ui/utils/statistics/computeConfusion.ts
@@ -5,12 +5,12 @@ const mapToRho = (binary: Array<number>) => binary.map(b => b === 0 ? -1 : 1)
 const computeNorm = (binaryRho: Array<number>) => Math.sqrt(binaryRho.reduce((acc, cur) => acc + Math.pow(cur, 2)))
 
 export const computeConfusion = (key: Uint8Array, outputBinary: Array<number>) => {
-  const keyBinaryRho = mapToRho(hexToBinary(key))
+  const keyBinary = hexToBinary(key)
+  const keyBinaryRho = mapToRho(keyBinary)
   const outputBinaryRho = mapToRho(outputBinary)
-  const n = 128
 
-  // TODO: pad outputBinaryRho for (let i = 0; i < keyBinaryRho.length - outputBinaryRho.length; i++) outputBinaryRho.push(0) // or push -1 ??
-  // n = outputBinaryRho.length
+  for (let i = 0; i < keyBinaryRho.length - outputBinaryRho.length; i++) outputBinaryRho.push(-1)
+  const n = outputBinaryRho.length
 
   const keyBinaryRhoNorm = computeNorm(keyBinaryRho)
   const outputBinaryRhoNorm = computeNorm(outputBinaryRho)
@@ -62,7 +62,31 @@ export const computeConfusionPhi = (key: Uint8Array, outputBinary: Array<number>
   const n1_ = n11 + n10
   const n0_ = n01 + n00
 
-  const phi = (n11 * n00 - n10 * n01) / Math.sqrt(n1_ * n0_ * n_0 * n_1)
+  const demoninator = Math.sqrt(n1_ * n0_ * n_0 * n_1)
+  const phi = demoninator === 0 ? 0 : (n11 * n00 - n10 * n01) / demoninator
 
   return Math.abs(phi) * 100
+}
+
+export const computeConfusionJaccardIndex = (key: Uint8Array, outputBinary: Array<number>) => {
+  const keyBinary = hexToBinary(key)
+
+  let n00 = 0
+  let n10 = 0
+  let n01 = 0
+  let n11 = 0
+
+  for (let i = 0; i < outputBinary.length; i++) {
+    const x = keyBinary[i]
+    const y = outputBinary[i]
+
+    if (x === 0 && y === 0) n00 += 1
+    if (x === 0 && y === 1) n01 += 1
+    if (x === 1 && y === 0) n10 += 1
+    if (x === 1 && y === 1) n11 += 1
+  }
+
+  const J = n11 / (n11 + n10 + n01)
+
+  return J * 100
 }
